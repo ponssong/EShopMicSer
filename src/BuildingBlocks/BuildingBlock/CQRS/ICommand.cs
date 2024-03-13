43e65a04b633b54ca20c2bf8a4c90f2e6ce9059f
ponsongs <ponsongvilay@outlook.com>
2024-03-13 12:44:32 -0500

    Catalog.API Docker

---
+++ src/BuildingBlocks/BuildingBlock/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+
+namespace BuildingBlock.CQRS
+{
+
+    public interface ICommand : ICommand<Unit>
+    {     
+    }
+
+    public interface ICommand<out TResponse>: IRequest<TResponse>
+    {
+    }
+}
