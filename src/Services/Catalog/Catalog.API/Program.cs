f0b525639cb6b40320ff8eff41fbf047e6d18e7a
ponsongs <ponsongvilay@outlook.com>
2024-06-04 17:06:28 -0500

    Global Handling Exceptions in ASP.NET Core with app.UseExceptionHandler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,8 @@
+using Amazon.Runtime;
 using BuildingBlock.Behaviors;
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,5 +29,33 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(excetptionHandler =>
+{
+    excetptionHandler.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Detail = exception.StackTrace,
+            Status = StatusCodes.Status500InternalServerError,
+            Instance = context.Request.Path
+        };
+
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
+
 
 app.Run();
