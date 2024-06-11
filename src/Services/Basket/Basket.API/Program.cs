76bee6faf92a7031b645545956bbf197da8c7b88
ponsongs <ponsongvilay@outlook.com>
2024-06-11 11:13:52 -0500

    Register MediatR and Carter libraries into Asp.Net Dependency Injection Service

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,21 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviorM<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 
 var app = builder.Build();
 
-// Configurwe the HTTP request pipeline
+// Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
