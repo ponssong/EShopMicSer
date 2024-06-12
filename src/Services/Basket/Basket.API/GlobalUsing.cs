2ee5ca2e06061aa450ac4618f90fd55a8ec56003
ponsongs <ponsongvilay@outlook.com>
2024-06-12 13:10:06 -0500

    Implement IBasketRepository Class using Marten IDocumentSession interface

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -5,3 +5,8 @@ global using Mapster;
 global using MediatR;
 global using FluentValidation;
 global using BuildingBlock.Behaviors;
+global using Marten;
+global using BuildingBlock.Exceptions;
+
+global using Basket.API.Exceptions;
+global using Marten;
