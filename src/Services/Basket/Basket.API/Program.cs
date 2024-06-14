54d06059dd5a10a0bbb048991c23b179e0023a9c
ponsongs <ponsongvilay@outlook.com>
2024-06-14 11:43:29 -0500

    Register Decorated CachedBasketRepository and StackExchangeRedis with Scrutor

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddMarten(opts =>
 
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 
