c62609a067e525f9bd2c79e84d82bb37b4c6b9d6
ponsongs <ponsongvilay@outlook.com>
2024-06-25 17:34:45 -0500

    Adding Dependency Injections Between Clean Architecture Layers

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,24 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+//-------------------
+// Infrastructure - EF core
+// Application - MediatR
+// API - Carter, Healthchecks
+
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
