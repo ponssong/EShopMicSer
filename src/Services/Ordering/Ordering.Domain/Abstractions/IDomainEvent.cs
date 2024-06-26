6aa8840abf810c84b28c74ae8463010fc19f7c35
ponsongs <ponsongvilay@outlook.com>
2024-06-26 11:40:33 -0500

    Tactical Domain-Driven Design: Entities, Value Objects, Aggregates, Agg. Roots

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    internal interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
