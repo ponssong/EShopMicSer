e0b4e52607e12fbe4ad3daf052df94783b5e94a2
ponsongs <ponsongvilay@outlook.com>
2024-09-16 13:05:46 -0500

    Developing Order Endpoints for REST APIs in Ordering.API Layer

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.API.Endpoints;
+
+//- Accepts pagination parameters.
+//- Constructs a GetOrdersQuery with these parameters.
+//- Retrieves the data and returns it in a paginated format.
+
+
+//public record GetOrdersRequest(PaginationRequest PaginationRequest);
+
+public class GetOrders
+{
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.API.Endpoints;
+
+
+//- Accepts a customer ID.
+//- Uses a GetOrdersByCustomerQuery to fetch orders.
+//- Returns the list of orders for that customer.
+
+//public record GetOrdersByCustomerRequest(Guid CustomerId);
+
+public class GetOrdersByCustomer
+{
+}
