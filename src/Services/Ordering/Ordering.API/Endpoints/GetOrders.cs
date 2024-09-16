1e6bfec9448ff85c7818886354100a3b3e8a4617
ponsongs <ponsongvilay@outlook.com>
2024-09-16 13:36:11 -0500

    Developing GetOrders Endpoint w/ Pagination for REST APIs in Ordering.API Layer

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,4 +1,7 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using BuildingBlock.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
 
 //- Accepts pagination parameters.
 //- Constructs a GetOrdersQuery with these parameters.
@@ -6,7 +9,25 @@
 
 
 //public record GetOrdersRequest(PaginationRequest PaginationRequest);
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
 
-public class GetOrders
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+
+            var response = result.Adapt<GetOrdersResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders")
+        .WithDescription("Get Orders");
+    }
 }
